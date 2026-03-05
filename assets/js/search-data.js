98847fd2a2f86ad450cf5278a63b867b442bd933
introspective-storm <anikaith198@gmail.com>
2026-03-05 06:33:28 +0000

    Deploying to gh-pages from @ introspective-storm/introspective-storm.github.io@76b5d767adc7ba49ca26da34034e4a1c1fc60b86 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-the-kernal",
+          title: "The Kernal",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/";
+            window.location.href = "/the-kernel/index.html";
           },
         },{id: "nav-publications",
           title: "publications",
